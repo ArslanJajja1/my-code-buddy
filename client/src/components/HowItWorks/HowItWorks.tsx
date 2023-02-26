8ffff47dc9fd590c181a5e09c2dca45e48f7dafa
arslanjajja1 <arslanjajja13@gmail.com>
2023-02-26 21:08:50 +0500

    How it works component setup

---
+++ client/src/components/HowItWorks/HowItWorks.tsx
@@ -0,0 +1,13 @@
+import useStyles from './styles'
+
+
+const HowItWorks = () => {
+    const classes = useStyles()
+  return (
+    <div>
+      How it works component !
+    </div>
+  )
+}
+
+export default HowItWorks
