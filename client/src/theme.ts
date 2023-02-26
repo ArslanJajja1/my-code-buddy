15969366b3fc3e3e8ca32a10d13f3e828516843c
arslanjajja1 <arslanjajja13@gmail.com>
2023-02-26 20:05:46 +0500

    modified default mui theme,added fontfamily

---
+++ client/src/theme.ts
@@ -0,0 +1,12 @@
+import { blue, grey,purple} from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    typography:{
+        fontFamily:'Sarabun',
+        fontWeightLight:300,
+        fontWeightMedium:400,
+        fontWeightRegular:500,
+        fontWeightBold:700
+    }
+})
