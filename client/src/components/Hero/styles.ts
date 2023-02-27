0e6f2f11ea6f5ba1da9e489e4ddef56c91cd8646
arslanjajja1 <arslanjajja13@gmail.com>
2023-02-27 07:45:12 +0500

    HowItWorks title desing improvements

--- client/src/components/Hero/styles.ts
+++ client/src/components/Hero/styles.ts
@@ -1,7 +1,13 @@
 import { makeStyles } from "@mui/styles";
 
+const SECONDARY_COLOR_MAIN = '#141125'
+
 export default makeStyles((theme)=>({
     container:{
         padding:'1px'
+    },
+    description:{
+        color:`${SECONDARY_COLOR_MAIN} !important`,
+        fontWeight:'600'
     }
 }))
--- client/src/components/HowItWorks/HowItWorks.tsx
+++ client/src/components/HowItWorks/HowItWorks.tsx
@@ -6,8 +6,8 @@ const HowItWorks = () => {
   return (
     <Container maxWidth='xl' sx={{my:'4rem'}}>
       <Box textAlign='center'>
-        <Typography variant='body1' component='h6' color='primary.dark' fontWeight='bold' letterSpacing='1px'>Start Using It in 3 Easy Steps !</Typography>
-        <Typography variant='h4' component='h2' fontWeight='bold' letterSpacing='1px'>How Does It Work ? </Typography>
+        <Typography sx={{typography:{xs:'p'},color:'primary.dark', fontWeight:'bold !important', letterSpacing:{md:'1px'}}} component='h6' >Start Using It in 3 Easy Steps !</Typography>
+        <Typography sx={{typography:{xs:'h5',sm:'h4'},fontWeight:'bold !important',letterSpacing:{md:'3px'}}} component='h2' >How Does It Work ? </Typography>
       </Box>
     </Container>
   )
