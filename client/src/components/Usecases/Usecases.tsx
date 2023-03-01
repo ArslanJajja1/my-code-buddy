6165da5acd0242f7cdbe3e12b6e99c3b070b42ab
arslanjajja1 <arslanjajja13@gmail.com>
2023-03-01 18:31:52 +0500

    usecases section setup

--- client/src/components/Usecases/Usecases.tsx
+++ client/src/components/Usecases/Usecases.tsx
@@ -1,76 +1,97 @@
- import BugIcon from '@mui/icons-material/BugReport';
-import CodeIcon from '@mui/icons-material/Code';
-import LegacyIcon from '@mui/icons-material/SettingsApplications';
-import LearningIcon from '@mui/icons-material/School';
-import OptimizationIcon from '@mui/icons-material/Tune';
-import TeamIcon from '@mui/icons-material/People';
-import FreelancerIcon from '@mui/icons-material/Work';
-import QualityIcon from '@mui/icons-material/Assessment';
-import CollaborationIcon from '@mui/icons-material/Group';
-import { useCases } from '../../data/usecasesData';
-import { Container, Box, Grid, Typography } from "@mui/material";
-import FeaturesCard from "../FeaturesCard/FeaturesCard";
-import UseCasesCard from '../Cards/UsecasesCard';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
+import HotelIcon from '@mui/icons-material/Hotel';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import Typography from '@mui/material/Typography';
 
-const icons = [<BugIcon />,<LegacyIcon/>,<CodeIcon/>,<LearningIcon/>,<OptimizationIcon/>,<TeamIcon/>,<FreelancerIcon/>,<CollaborationIcon/>,<QualityIcon/>]
-const Usecases = () => {
+const Usecases = ()=> {
   return (
-    <Container maxWidth="xl">
-      <Box textAlign="center">
-        <Typography
-          sx={{
-            typography: { xs: "p" },
-            color: "primary.dark",
-            fontWeight: "bold !important",
-            letterSpacing: { md: "1px" },
-          }}
-          component="h6"
+    <Timeline position="alternate">
+      <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          align="right"
+          variant="body2"
+          color="text.secondary"
         >
-          Effortlessly Improve Your Code with CodeBuddy Tools !
-        </Typography>
-        <Typography
-          sx={{
-            typography: { xs: "h5", sm: "h4" },
-            fontWeight: "bold !important",
-            letterSpacing: { md: "3px" },
-          }}
-          component="h2"
+          9:30 am
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot>
+            <FastfoodIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            Eat
+          </Typography>
+          <Typography>Because you need strength</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      
+      <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          variant="body2"
+          color="text.secondary"
         >
-          Usecases
-        </Typography>
-      </Box>
-      <Grid
-        container
-        spacing={{ xs: 2 }}
-        sx={{ justifyContent: "center", alignItems: "center" }}
-        mx="auto"
-        py={4}
-      >
-        {
-            useCases.map((usecase,index)=>(
-                <Grid
-                item
-                xs={12}
-                md={4}
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-                key={index+10}
-                textAlign='center' justifyContent='center' alignItems='center'
-              >
-                <UseCasesCard
-                  title={usecase.title}
-                  description={usecase.description}
-                  icon={icons[index]}
-                />
-              </Grid>
-            ))
-        }        
-      </Grid>
-    </Container>
-  )
+          10:00 am
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot color="primary">
+            <LaptopMacIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            Code
+          </Typography>
+          <Typography>Because it&apos;s awesome!</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector />
+          <TimelineDot color="primary" variant="outlined">
+            <HotelIcon />
+          </TimelineDot>
+          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            Sleep
+          </Typography>
+          <Typography>Because you need rest</Typography>
+        </TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+          <TimelineDot color="secondary">
+            <RepeatIcon />
+          </TimelineDot>
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }}>
+          <Typography variant="h6" component="span">
+            Repeat
+          </Typography>
+          <Typography>Because this is the life you love!</Typography>
+        </TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
 }
 
 export default Usecases
