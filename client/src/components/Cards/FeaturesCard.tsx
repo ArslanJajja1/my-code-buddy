1ed375fab2f52d6a6d791d299314e4dfc13858f3
arslanjajja1 <arslanjajja13@gmail.com>
2023-03-03 11:17:09 +0500

    features section responsiveness,formatting

--- client/src/components/Cards/FeaturesCard.tsx
+++ client/src/components/Cards/FeaturesCard.tsx
@@ -1,25 +1,41 @@
-import { Card, CardContent, CardHeader, Typography,IconButton, Grid,CardActions,CardMedia,Button,Avatar} from '@mui/material';
-import CodeIcon from '@mui/icons-material/Code';
-type propsTypes= {
-    title:string,
-    description:string,
-    way:number,
-    Icon:React.ReactNode
-}
+import {
+  Card,
+  CardContent,
+  Typography,
+  IconButton,
+} from "@mui/material";
 
-const FeaturesCard = ({title,description,way,Icon}:propsTypes) => {
+type propsTypes = {
+  title: string;
+  description: string;
+  way: number;
+  Icon: React.ReactNode;
+};
+
+const FeaturesCard = ({ title, description, way, Icon }: propsTypes) => {
   return (
-    <Card sx={{ maxWidth: 345,minHeight:200 }}>
-              
+    <Card sx={{ maxWidth: 345, minHeight: 250 }}>
       <CardContent>
-        <IconButton color='primary' size='large' >
-            {Icon}
+        <IconButton color="primary" size="large">
+          {Icon}
         </IconButton>
-        <Typography variant='body2' letterSpacing={3} fontWeight='bold' color='primary.dark' >Way {way}</Typography>
-        <Typography gutterBottom letterSpacing='1px !important' sx={{typography:{xs:'h6',md:'h5'}}} component="h5">
+        <Typography
+          variant="body2"
+          letterSpacing={3}
+          fontWeight="bold"
+          color="primary.dark"
+        >
+          Way {way}
+        </Typography>
+        <Typography
+          gutterBottom
+          letterSpacing="1px !important"
+          sx={{ typography: { xs: "h6", md: "h5" } }}
+          component="h5"
+        >
           {title}
         </Typography>
-        <Typography variant="body2" color="text.secondary" textAlign='justify'>
+        <Typography variant="body2" color="text.secondary" textAlign="justify">
           {description}
         </Typography>
       </CardContent>
@@ -28,8 +44,7 @@ const FeaturesCard = ({title,description,way,Icon}:propsTypes) => {
         <Button size="small">Learn More</Button>
       </CardActions> */}
     </Card>
-  )
-}
+  );
+};
 
-export default FeaturesCard
-
+export default FeaturesCard;
--- client/src/components/Features/Fearures.tsx
+++ client/src/components/Features/Fearures.tsx
@@ -1,60 +1,55 @@
 import { Container, Box, Grid, Typography } from "@mui/material";
 import FeaturesCard from "../Cards/FeaturesCard";
-import ErrorIcon from '@mui/icons-material/Error';
- import CloudSyncIcon from '@mui/icons-material/CloudSync';
- import BugReportIcon from '@mui/icons-material/BugReport';
- import HelpIcon from '@mui/icons-material/Help';
- import SpeedIcon from '@mui/icons-material/Speed';
- import AutorenewIcon from '@mui/icons-material/Autorenew';
- 
+import ErrorIcon from "@mui/icons-material/Error";
+import CloudSyncIcon from "@mui/icons-material/CloudSync";
+import BugReportIcon from "@mui/icons-material/BugReport";
+import HelpIcon from "@mui/icons-material/Help";
+import SpeedIcon from "@mui/icons-material/Speed";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
+
 const featuresData = [
   {
     id: 0,
     title: "Code correction",
     description:
       "CodeBuddy uses  natural language processing and machine learning to identify and suggest fixes for syntax errors, typos, and other coding issues, helping users write error-free code.",
-      icon: <ErrorIcon sx={{fontSize:'3rem'}}/>
-
+    icon: <ErrorIcon sx={{ fontSize: "3rem" }} />,
   },
   {
     id: 1,
     title: "Language conversion",
     description:
       "CodeBuddy enables users to convert code from one programming language to another, facilitating code integration and making it easier for users to switch between languages.",
-      icon: <CloudSyncIcon sx={{fontSize:'3rem'}}/>
+    icon: <CloudSyncIcon sx={{ fontSize: "3rem" }} />,
   },
   {
     id: 2,
     title: "Bug detection",
     description:
       "CodeBuddy uses AI algorithms to detect potential bugs in code, providing users with a warning before issues arise and helping them avoid costly mistakes.",
-      icon: <BugReportIcon sx={{fontSize:'3rem'}}/>
-
+    icon: <BugReportIcon sx={{ fontSize: "3rem" }} />,
   },
   {
     id: 3,
     title: "Code explanation",
     description:
       "CodeBuddy provides automated explanations for complex code snippets, making it easier for users to understand the code's functionality and purpose.",
-      icon: <HelpIcon sx={{fontSize:'3rem'}}/>
-
+    icon: <HelpIcon sx={{ fontSize: "3rem" }} />,
   },
   {
     id: 4,
     title: "Code optimization",
     description:
       "CodeBuddy can analyze code and suggest optimizations, such as removing redundant code, simplifying complex expressions, and improving performance.",
-      icon: <SpeedIcon sx={{fontSize:'3rem'}}/>
-
+    icon: <SpeedIcon sx={{ fontSize: "3rem" }} />,
   },
   {
     id: 5,
     title: "AI-assisted code writing",
     description:
       "TCodeBuddy offers AI-assisted code writing, suggesting code snippets and auto-completing code based on the user's input, helping users save time and write code more efficiently.",
-      icon: <AutorenewIcon sx={{fontSize:'3rem'}}/>
-
-  }
+    icon: <AutorenewIcon sx={{ fontSize: "3rem" }} />,
+  },
 ];
 const Features = () => {
   return (
@@ -85,33 +80,33 @@ const Features = () => {
       <Grid
         container
         spacing={{ xs: 2 }}
-        sx={{ justifyContent: "center", alignItems: "center" }}
+        sx={{ justifyContent: "center", alignItems: "center",flexWrap:'wrap' }}
         mx="auto"
         py={4}
       >
-        {
-            featuresData.map((feature)=>(
-                <Grid
-                item
-                xs={12}
-                md={4}
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-                key={feature.id}
-                textAlign='center' justifyContent='center' alignItems='center'
-              >
-                <FeaturesCard
-                  title={feature.title}
-                  way={feature.id+1}
-                  description={feature.description}
-                  Icon={feature.icon}
-                />
-              </Grid>
-            ))
-        }        
+        {featuresData.map((feature) => (
+          <Grid
+            item
+            xs={12}
+            md={4}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            key={feature.id}
+            textAlign="center"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <FeaturesCard
+              title={feature.title}
+              way={feature.id + 1}
+              description={feature.description}
+              Icon={feature.icon}
+            />
+          </Grid>
+        ))}
       </Grid>
     </Container>
   );
