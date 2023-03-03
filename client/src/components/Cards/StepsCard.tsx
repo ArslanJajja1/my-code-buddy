3b77108ea7a5251318687635da72466a061187f8
arslanjajja1 <arslanjajja13@gmail.com>
2023-03-03 11:31:41 +0500

    improving responsiveness of the steps sections

--- client/src/components/Cards/StepsCard.tsx
+++ client/src/components/Cards/StepsCard.tsx
@@ -1,32 +1,47 @@
-import { Card, CardContent, CardHeader, Typography, Grid,CardActions,CardMedia,Button } from '@mui/material';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+  Grid,
+  CardActions,
+  CardMedia,
+  Button,
+} from "@mui/material";
 
-type propsTypes= {
-    title:string,
-    description:string,
-    step:string,
-    imageUrl:string
-}
-const StepsCard = ({title,description,step,imageUrl}:propsTypes) => {
+type propsTypes = {
+  title: string;
+  description: string;
+  step: string;
+  imageUrl: string;
+};
+const StepsCard = ({ title, description, step, imageUrl }: propsTypes) => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={imageUrl}
-        title="green iguana"
-      />
+    <Card sx={{ maxWidth: 345, minHeight: 250 }}>
+      <CardMedia sx={{ height: 140 }} image={imageUrl} title="green iguana" />
       <CardContent>
-        <Typography variant='body2' letterSpacing={3} fontWeight='bold' color='primary.dark' >Step {step}</Typography>
-        <Typography gutterBottom letterSpacing='1px !important' sx={{typography:{xs:'h6',md:'h5'}}} component="h5">
-          {title}
+        <Typography
+          variant="body2"
+          letterSpacing={3}
+          fontWeight="bold"
+          color="primary.dark"
+        >
+          Step {step}
         </Typography>
-        <Typography variant="body2" color="text.secondary" textAlign='justify'>
-          {description}
+        <Typography
+          gutterBottom
+          letterSpacing="1px !important"
+          sx={{ typography: { xs: "h6", md: "h5" } }}
+          component="h5"
+        >
+          {title.length > 20 ? title.substring(0, 20) + "..." : title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" textAlign="justify">
+          {description.length > 100
+            ? description.substring(0, 100) + "..."
+            : description}
         </Typography>
       </CardContent>
-      {/* <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions> */}
     </Card>
   );
 };
--- client/src/components/HowItWorks/HowItWorks.tsx
+++ client/src/components/HowItWorks/HowItWorks.tsx
@@ -1,22 +1,61 @@
-import useStyles from './styles'
-import { Stack, Box, Container, Typography, Grid } from '@mui/material'
-import StepsCard from '../Cards/StepsCard'
+import { Stack, Box, Container, Typography, Grid } from "@mui/material";
+import StepsCard from "../Cards/StepsCard";
 
 const HowItWorks = () => {
-  const classes = useStyles()
   return (
-    <Container maxWidth='xl' sx={{ py:8 }}>
-      <Box textAlign='center'>
-        <Typography sx={{ typography: { xs: 'p' }, color: 'primary.dark', fontWeight: 'bold !important', letterSpacing: { md: '1px' } }} component='h6'>Start Using It in 3 Easy Steps !</Typography>
-        <Typography sx={{ typography: { xs: 'h5', sm: 'h4' }, fontWeight: 'bold !important', letterSpacing: { md: '3px' } }} component='h2'>How Does It Work ?</Typography>
+    <Container maxWidth="xl" sx={{ py: 8 }}>
+      <Box textAlign="center">
+        <Typography
+          sx={{
+            typography: { xs: "p" },
+            color: "primary.dark",
+            fontWeight: "bold !important",
+            letterSpacing: { md: "1px" },
+          }}
+          component="h6"
+        >
+          Start Using It in 3 Easy Steps !
+        </Typography>
+        <Typography
+          sx={{
+            typography: { xs: "h5", sm: "h4" },
+            fontWeight: "bold !important",
+            letterSpacing: { md: "3px" },
+          }}
+          component="h2"
+        >
+          How Does It Work ?
+        </Typography>
       </Box>
-      <Stack direction={{xs:'column',md:'row'}} textAlign='center' justifyContent='center' alignItems='center' spacing={8} py={4}>
-        <StepsCard title='Select a CodeBuddy Tool' step = '1' imageUrl='https://cdn.pixabay.com/photo/2018/05/08/08/44/artificial-intelligence-3382507_960_720.jpg' description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quaerat recusandae quos at! Natus voluptatem placeat nihil quas itaque eum!'/>
-        <StepsCard title='Add The Required Info' step='2' imageUrl='https://cdn.pixabay.com/photo/2016/11/02/08/56/binary-1790842_960_720.jpg' description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quaerat recusandae quos at! Natus voluptatem placeat nihil quas itaque eum!'/>
-        <StepsCard title='Get The Desired Output' step='3' imageUrl='https://cdn.pixabay.com/photo/2018/12/02/10/07/web-3850917_960_720.jpg' description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quaerat recusandae quos at! Natus voluptatem placeat nihil quas itaque eum!'/>
+      <Stack
+        direction={{ xs: "column", md: "row" }}
+        textAlign="center"
+        justifyContent="center"
+        alignItems="center"
+        spacing={8}
+        py={4}
+      >
+        <StepsCard
+          title="Select a Tool"
+          step="1"
+          imageUrl="https://cdn.pixabay.com/photo/2018/05/08/08/44/artificial-intelligence-3382507_960_720.jpg"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quaerat recusandae quos at! Natus voluptatem placeat nihil quas itaque eum!"
+        />
+        <StepsCard
+          title="Add Info"
+          step="2"
+          imageUrl="https://cdn.pixabay.com/photo/2016/11/02/08/56/binary-1790842_960_720.jpg"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quaerat recusandae quos at! Natus voluptatem placeat nihil quas itaque eum!"
+        />
+        <StepsCard
+          title="Get Output"
+          step="3"
+          imageUrl="https://cdn.pixabay.com/photo/2018/12/02/10/07/web-3850917_960_720.jpg"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quaerat recusandae quos at! Natus voluptatem placeat nihil quas itaque eum!"
+        />
       </Stack>
     </Container>
-  )
-}
+  );
+};
 
-export default HowItWorks
+export default HowItWorks;
