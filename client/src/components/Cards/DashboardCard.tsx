005317693f058004c97435748d2251e4c6a6b763
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-02 14:13:52 +0500

    dashboard cards design

--- client/src/components/Cards/DashboardCard.tsx
+++ client/src/components/Cards/DashboardCard.tsx
@@ -1,19 +1,29 @@
 import {Box,Stack,Typography,IconButton} from '@mui/material'
-import {CodeOff} from '@mui/icons-material';
-const DashboardCard = () => {
+import {CodeOff,ArrowOutward} from '@mui/icons-material';
+
+type propTypes = {
+    title:string,
+    description:string,
+    icon : React.ReactNode
+}
+const DashboardCard = ({title,description,icon}:propTypes) => {
   return (
-    <Box sx={{width:'12rem',height:'12rem',p:"1rem",backgroundColor:"skyblue"}}>
-        <Stack direction='row' sx={{d:'flex',justifyContent:'space-between',pb:'.3rem'}}>
-            <IconButton>
-                <CodeOff />
+    <Box sx={{width:'15rem',height:'12rem',p:"1rem",backgroundColor:"white"}}>
+        <Stack direction='row' sx={{d:'flex',justifyContent:'space-between',pb:'.6rem'}}>
+            <IconButton color="primary">
+                {icon}
             </IconButton>
             <IconButton>
-                <CodeOff />
+                <ArrowOutward />
             </IconButton>
         </Stack>
         <Stack spacing={1}>
-            <Typography variant='h6' component='h3'>Title </Typography>
-            <Typography variant='body1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, rem!</Typography>
+            <Typography variant='body1' component='h3' sx={{fontWeight:'600',letterSpacing:1}}>
+            {title.length>25 ? title.substring(0,20) + '...':title}
+            </Typography>
+            <Typography variant='body1' component='p' sx={{color:'grayText'}}>
+            {description.length> 75 ? description.substring(0,75) + '...' : description}
+            </Typography>
         </Stack>
     </Box>
   )
