3f396ff7e03048d9d7791f4a3b0f5cae3fca4ce1
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-02 12:36:43 +0500

    dashboard page setup

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,17 +1,18 @@
-import {Box,Stack,Container,Grid, Divider} from '@mui/material'
+import { Box, Stack, Container, Grid, Divider } from '@mui/material'
 import Navbar from '../../components/Navbar/Navbar'
+import DashboardCard from '../../components/Cards/DashboardCard'
 
 const Dashboard = () => {
   return (
-    <Box>
-        <Container maxWidth='xl'>
-         <Navbar dashboard={true}/>
-            <Divider />
-            <Stack color={'black'} mt={10}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum tempora autem illum iusto temporibus ipsa aut. Ad assumenda sed, laboriosam voluptatibus molestiae ullam laudantium cupiditate aut magni quisquam repudiandae iste.
-            </Stack>
-        </Container>
-    </Box>
+    <>
+    <Navbar dashboard={true} />
+        <Divider />
+    <Container maxWidth='xl' sx={{marginTop:"5rem"}}>
+      <Box>
+        <DashboardCard />
+      </Box>
+    </Container>
+    </>
   )
 }
 
