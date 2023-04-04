3e0373bab6d11e9734c5226ba2cb41df4d4a785e
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-04 15:45:07 +0500

    code correction form design

--- client/src/pages/Tools/CodeCorrection/CodeCorrectionForm.tsx
+++ client/src/pages/Tools/CodeCorrection/CodeCorrectionForm.tsx
@@ -1,4 +1,4 @@
-import {Stack,Box,Typography,TextField,MenuItem} from '@mui/material'
+import {Button,Box,Typography,TextField,MenuItem} from '@mui/material'
 
 
 const languages = [
@@ -39,17 +39,22 @@ const CodeCorrectionForm = () => {
     <Box
     component="form"
     sx={{
-      '& .MuiTextField-root': { m: 1, width: '25ch' },
+        backgroundColor:"white",
+        padding:5,
+        my:5,
+        width:"50%",
+        mx:"auto"
     }}
-    noValidate
-    autoComplete="off"
   >
      <TextField
-          id="outlined-select-currency"
+        //   id="outlined-select-currency"
           select
           label="Coding language"
           defaultValue="JavaScript"
           helperText="Please select language"
+          sx={{
+            width:"100%"
+          }}
         >
           {languages.map((option) => (
             <MenuItem key={option} value={option}>
@@ -57,6 +62,31 @@ const CodeCorrectionForm = () => {
             </MenuItem>
           ))}
         </TextField>
+        <TextField
+        //   id="outlined-multiline-static"
+          label="What correction ? "
+          placeholder='What correction you want in your code ? Explain in detail in this box to get the best results .'
+          multiline
+          rows={4}
+          defaultValue="" 
+          sx={{
+            width:"100%",
+            mt:5
+          }}
+          />
+           <TextField
+          label="Paste Code "
+          placeholder='Paste the code here that you want to be corrected !'
+          multiline
+          rows={4}
+          defaultValue="" 
+          sx={{
+            width:"100%",
+            my:5
+          }}
+          />
+        <Button color='primary' sx={{ fontSize: { xs: "0.6rem", sm: "1rem",width:"100%"} }} variant="contained">Correct this code</Button>
+
   </Box>
   )
 }
